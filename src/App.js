19f6e06c461c98ccffabeda3fca6046584bb4ac2
nitnkmr <nitin84498@gmail.com>
2023-02-15 20:32:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Intro from './component/Intro';
+
+function App() {
+  return (
+    <div className="App">
+      <Intro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Intro.jsx
@@ -0,0 +1,12 @@
+
+function Intro(){
+    return(
+        <div className="intro">
+        <h1>The Evalution of Airbnb's FrontEnd</h1>
+        <h4>@Spiketern</h4>
+        <img src="https://venturebeat.com/wp-content/uploads/2014/07/airbnb-logo-red.jpg?w=1200&strip=all" alt="" />
+        <p>Airbnb</p>
+    </div>
+    );
+}
+export default Intro;
